9d4a7ba15d9044e9c950dabd799814f6ad29b53a
SHAHGHOLI-MAHMOOD <SHAHGHOLI-MAHMOOD@users.noreply.github.com>
2020-07-06 17:17:16 +0430

    **** ADDING waitx() SYSTEM CALL TO CALCULATE WAITING TIME AND RESPONSE TIME ~~~

--- waitx.c
+++ waitx.c
@@ -6,17 +6,21 @@
 int main (int argc,char *argv[])
 {
   int pid;
+  int waiting_time, running_time, waitx_pid;
 
-  pid = fork ();
+  pid = fork();
+  
   if (pid == 0)
     {	
     exec(argv[1],argv);
     printf(1, "execution of New Process %s failed\n", argv[1]);
     }
+
   else
   {
-    /* code */
+    waitx_pid = waitx(&waiting_time,&running_time);
+    printf(1, "Waiting Time = %d\nRuning Time = %d\nProcess id: %d \n",waiting_time,running_time,waitx_pid);
   }
-  
+
   exit();
 }
