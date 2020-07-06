a231ef476d3630e199d9bf4df0a1459e4119cc9e
SHAHGHOLI-MAHMOOD <SHAHGHOLI-MAHMOOD@users.noreply.github.com>
2020-07-06 16:26:20 +0430

    ## CHECKING FORK() , EXEC() AND PRINTF().

--- waitx.c
+++ waitx.c
@@ -5,5 +5,18 @@
 
 int main (int argc,char *argv[])
 {
+  int pid;
+
+  pid = fork ();
+  if (pid == 0)
+    {	
+    exec(argv[1],argv);
+    printf(1, "execution of New Process %s failed\n", argv[1]);
+    }
+  else
+  {
+    /* code */
+  }
+  
   exit();
 }
